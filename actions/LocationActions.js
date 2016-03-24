1ba66188f494b0eb92893db5097c39388111360e
savioabuga <savioabuga@gmail.com>
2016-03-24 09:08:54 +0300

    Getting started

---
+++ actions/LocationActions.js
@@ -0,0 +1,11 @@
+var alt = require('../alt');
+
+class LocationActions {
+    updateLocations(locations) {
+        return locations;
+    }
+    
+    
+}
+
+module.exports = alt.createActions(LocationActions);
---
+++ stores/LocationStore.js
@@ -0,0 +1,8 @@
+var alt = require('../alt');
+class LocationStore {
+    constructor(){
+        
+    }
+    
+    
+}
